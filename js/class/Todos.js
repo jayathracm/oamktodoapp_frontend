30d25ad97efd0f1cc0944c06776f5cb5fe00f991
jayathracm <jayathracm@gmail.com>
2024-03-24 00:45:47 +0200

    test 24th

--- js/class/Todos.js
+++ js/class/Todos.js
@@ -1,3 +1,85 @@
+// import { Task } from './Task.js';
+
+// class Todos {
+//     #tasks = [];
+//     #backend_url = '';
+
+//     constructor(url) {
+//         this.#backend_url = url;
+//     }
+
+//     getTasks() {
+//         return new Promise(async (resolve, reject) => {
+//             fetch(this.#backend_url)
+//                 .then(response => response.json())
+//                 .then(json => {
+//                     this.#readJSON(json);
+//                     resolve(this.#tasks);
+//                 },(error) => {
+//                     reject(error);
+//                 })
+//      })
+
+//     }
+
+//     #readJSON = (taskAsJson) => {
+//         taskAsJson.forEach(node => {
+//             const task = new Task(node.id, node.description);
+//             this.#tasks.push(task);
+//         });
+//     }
+
+//     #addToArray = (id, text) => {
+//         const task = new Task(id, text);
+//         this.#tasks.push(task);
+//         return task;
+//     }
+
+//     addTask = (text) => {
+//         return new Promise(async (resolve, reject) => {
+//             const json = JSON.stringify({description: text});
+//             fetch(this.#backend_url + "/new", {
+//                 method: 'POST',
+//                 headers: {
+//                     'Content-Type': 'application/json'
+//                 },
+//                 body: json
+//             })
+//             .then(response => response.json())
+//             .then(json => {
+//                 resolve(this.#addToArray(json.id, text));
+//                 console.log(error)
+//             }, (error) => {
+//                 reject(error);
+//                 console.log(error)
+//             })
+//         })
+//     }
+
+//     removeTask = (id) => {
+//         return new Promise(async (resolve, reject) => {
+//             fetch(this.#backend_url + "/delete/" + id, {
+//                 method: 'DELETE'
+//             })
+//             .then(response => response.json())
+//             .then(json => {
+//                 this.#removeFromArray(json.id);
+//                 resolve(json.id);
+//             }, (error) => {
+//                 reject(error);
+//             })
+//         })
+//     }
+
+//     #removeFromArray = (id) => {
+//         const arrayWithoutRemoved = this.#tasks = this.#tasks.filter(task => task.id !== id)
+//         this.#tasks = arrayWithoutRemoved;
+//     }
+
+// }
+
+// export { Todos }
+
 import { Task } from './Task.js';
 
 class Todos {
@@ -8,22 +90,21 @@ class Todos {
         this.#backend_url = url;
     }
 
-    getTasks() {
-        return new Promise(async (resolve, reject) => {
+    getTasks = () => {
+        return new Promise(async(resolve, reject) => {
             fetch(this.#backend_url)
-                .then(response => response.json())
-                .then(json => {
-                    this.#readJSON(json);
-                    resolve(this.#tasks);
-                },(error) => {
-                    reject(error);
-                })
-     })
-
+            .then((response) => response.json())
+            .then((json) => {
+                this.#readJson(json)
+                resolve(this.#tasks);
+            }, error => {
+                reject(error);
+            })
+        })
     }
 
-    #readJSON = (taskAsJson) => {
-        taskAsJson.forEach(node => {
+    #readJson = (tasksAsJson) => {
+        tasksAsJson.forEach(node => {
             const task = new Task(node.id, node.description);
             this.#tasks.push(task);
         });
@@ -36,7 +117,7 @@ class Todos {
     }
 
     addTask = (text) => {
-        return new Promise(async (resolve, reject) => {
+        return new Promise(async(resolve, reject) => {
             const json = JSON.stringify({description: text});
             fetch(this.#backend_url + "/new", {
                 method: 'POST',
@@ -45,35 +126,36 @@ class Todos {
                 },
                 body: json
             })
-            .then(response => response.json())
-            .then(json => {
+            .then((response) => response.json())
+            .then((json) => {
                 resolve(this.#addToArray(json.id, text));
-                console.log(error)
-            }, (error) => {
+
+            }, error => {
                 reject(error);
-                console.log(error)
             })
-        })
-    }
+    })
+}
 
     removeTask = (id) => {
-        return new Promise(async (resolve, reject) => {
+        return new Promise(async(resolve, reject) => {
             fetch(this.#backend_url + "/delete/" + id, {
                 method: 'DELETE'
             })
-            .then(response => response.json())
-            .then(json => {
-                this.#removeFromArray(json.id);
+            .then((response) => response.json())
+            .then((json) => {
+                this.#removeFromArray(id);
                 resolve(json.id);
-            }, (error) => {
+            }, error => {
+                console.error('Error:', error);
                 reject(error);
             })
         })
     }
 
     #removeFromArray = (id) => {
-        const arrayWithoutRemoved = this.#tasks = this.#tasks.filter(task => task.id !== id)
+        const arrayWithoutRemoved = this.#tasks.filter(task => task.id !== id);
         this.#tasks = arrayWithoutRemoved;
+        // return id;
     }
 
 }
